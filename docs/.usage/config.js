4edc7ec8378fa67c599f5f84d250abb731309c77
ewan <wane@qq.com>
2021-04-13 21:02:02 +0800

    v

--- docs/.usage/config.js
+++ docs/.usage/config.js
@@ -1,7 +1,10 @@
 module.exports.config = {
     title: '标题文本',
     description: '说明文本',
+    
+    // 默认主题配置
     themeConfig: {
+        logo: '/logo.png', // 公共资源库：docs/.vuepress/public/
         nav: [
             { text: '指南', link: '/' },
         ],
