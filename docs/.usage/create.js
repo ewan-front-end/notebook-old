8b047a7446fac6cea4aa2ed3908069aa046423a6
wanyuaning <wanyuaning@163.com>
2021-05-14 01:22:41 +0800

    v

--- docs/.usage/create.js
+++ docs/.usage/create.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const _path = require('path')
-const homeObj = {key:'ROOT_HOME', id: '', path: '.', title: '首页标题'}
 const siteMap = require('./siteMap')
 const R_D__ = _path.resolve(__dirname, '../')
 const R_D_I = _path.resolve(R_D__, 'README.md')
@@ -8,14 +7,13 @@ const R_D_I = _path.resolve(R_D__, 'README.md')
 /**
  * {home_tools: {
  *     parent:homeObj,          // 拼接path
- *     key:'tools', 
+ *     key:'tools',             // 暂时无作用
  *     name:'tools或自定义',     // 链接[name](path)
- *     id:'home_tools', 
+ *     id:'home_tools',          // 用于从flatDataMap中查询数据
  *     path:'绝对路径docs/tools' // 链接[name](path)
  * }}
  */
-let flatDataMap = {},
-homePageStr,          // 首页模板
+let flatDataMap = {}, // 第一次扁平化数据
 indexLinks = ``       // 首页链接拼接：[key||name](path)
 
 const createFile = (item) => {
@@ -25,7 +23,10 @@ const createFile = (item) => {
     flatDataMap[id] = {type: 'FILE', path: _path.resolve(R_D__, item.path), target: item}    
 }, createDir = (item, children) => {
     const {id, path} = item, absolutePath = _path.resolve(R_D__, path)
-    if (!fs.existsSync(absolutePath)) {flatDataMap[id] = {type: 'DIRE', path: absolutePath, target: item}; flatDataMap[id + '_index'] = {type: 'FILE', path: _path.resolve(absolutePath, 'README.md'), target: item}}
+    if (!fs.existsSync(absolutePath)) {
+        flatDataMap[id] = {type: 'DIRE', path: absolutePath, target: item}; 
+        flatDataMap[id + '/index'] = {type: 'FILE', path: _path.resolve(absolutePath, 'README.md'), target: item}
+    }
     for (key in children) { handleItem(key, children[key], item) }
 }, handleItem = (key, item, parent) => {
     const {CHILDREN} = item, 
@@ -39,15 +40,10 @@ const createFile = (item) => {
 }
 for (key in siteMap) {
     let item = siteMap[key]    
-    handleItem(key, item, homeObj)
+    handleItem(key, item, {key:'ROOT_HOME', id: '', path: '.', title: '首页标题'})
     // 首页一级导航
     indexLinks += `[${item.linkName}](${item.path}) | `
 }
-
-homePageStr = `---
-home: true\nheroImage: /hero.png\nheroText: Hero 标题\ntagline: Hero 副标题\nactionText: 快速上手 →\nactionLink: /zh/guide/\nfeatures:\n- title: 简洁至上\n  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动\n  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。\n- title: 高性能\n  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You\n---\n${indexLinks}`
 
 // 构建开始 flatDataMap{{type: 'DIRE', target: {data}, path: 'D:\\Ewan\\Hello\\aa\\docs\\tools'}, {type: 'FILE', target: {data}, path: 'D:\\Ewan\\Hello\\aa\\docs\\tools/README.md'}}
 for (id in flatDataMap) {
@@ -88,4 +84,9 @@ for (id in flatDataMap) {
         if (fs.existsSync(path)) {} else {fs.writeFile(path, content, { encoding: 'utf8' }, err => { console.log('created ' + path) })}
     }
 }
+// 首页模板
+const homePageStr = `---
+home: true\nheroImage: /hero.png\nheroText: Hero 标题\ntagline: Hero 副标题\nactionText: 快速上手 →\nactionLink: /zh/guide/\nfeatures:\n- title: 简洁至上\n  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动\n  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。\n- title: 高性能\n  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: MIT Licensed | Copyright © 2018-present Evan You\n---\n${indexLinks}`
 if (fs.existsSync(R_D_I)) { } else {fs.writeFile(R_D_I, homePageStr, { encoding: 'utf8' }, err => { console.log('created ' + R_D_I) })}
