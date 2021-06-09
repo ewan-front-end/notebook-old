16fac6bf6cc5605560a2f87a0dac5b3eca82c0ed
wanyuaning <wanyuaning@163.com>
2021-06-09 20:56:44 +0800

    v

--- docs/.usage/create.js
+++ docs/.usage/create.js
@@ -3,6 +3,7 @@ const path = require('path')
 const siteMap = require('./siteMap')
 const generateFile = require('./generateFile')
 const argArr = process.argv.slice(2)
+let readmeDataMap = {}
 const handleItemChildren = (item, children) => {
     mkdirSync(path.resolve(__dirname, '..'+item.path))
     readmeDataMap[item.path + '/README'] = {path: item.path + '/README', target: item}
@@ -38,7 +39,7 @@ if (argArr.length > 0) {
         handleItem(key, item, DATA_ROOT_)
     }
 } else {
-    let readmeDataMap = {}, indexLinks = ``
+    let indexLinks = ``
     
     for (key in siteMap) {
         let item = siteMap[key]  
@@ -49,8 +50,8 @@ if (argArr.length > 0) {
     // 写首页文件
     const homePageStr = `---\nhome: true\nheroImage: /hero.png\nheroText: Hero 标题\ntagline: Hero 副标题\nactionText: 快速上手 →\nactionLink: /zh/guide/\nfeatures:\n- title: 简洁至上\n  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。\n- title: Vue驱动\n  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。\n- title: 高性能\n  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。\nfooter: MIT Licensed | Copyright © 2018-present Evan You\n---\n${indexLinks}`
     writeFile(path.resolve(__dirname, '../README.md'), homePageStr)
-    for (id in readmeDataMap) {
-        let {path, target} = readmeDataMap[id]
-        generateFile(path, target)
-    }
+}
+for (id in readmeDataMap) {
+    let {path, target} = readmeDataMap[id]
+    generateFile(path, target)
 }
