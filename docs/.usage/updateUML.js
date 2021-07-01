8a4bb5207b3f714ff009a4b84209b7c476dd944e
wanyuaning <wanyuaning@163.com>
2021-07-01 21:00:15 +0800

    v

--- docs/.usage/updateUML.js
+++ docs/.usage/updateUML.js
@@ -2,9 +2,15 @@ const fs = require('fs')
 const {saveFile} = require('./src/tools-fs')
 const plantuml = require('node-plantuml')
 const DATA_UML = require('./data/.PLANTUML')
-
+// 参考：/node#fs模块读取流/写入流/管道流
 for (let key in DATA_UML) {//{charset:'UTF-8'}
-    plantuml.generate(DATA_UML[key]).out.pipe(fs.createWriteStream('./docs/.vuepress/public/uml/' + key + '.png', {defaultEncoding: 'utf8'}))
+    const rs = plantuml.generate(DATA_UML[key], {charset: 'UTF-8'})
+    const ws = fs.createWriteStream('./docs/.vuepress/public/uml/' + key + '.png')
+    rs.out.pipe(ws)
+    rs.on('end', () => {
+        console.log(key + '.png 文件创建完成')
+        ws.end()
+    })
 }
     
     
